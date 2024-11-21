d6556f82cdbc83dbb2786a375444a068086c5e7f
Rakib48-code <shahidulhuda0@gmail.com>
2024-11-21 10:40:00 +0600

    add _rec_name

--- hospital_management/models/appointment.py
+++ hospital_management/models/appointment.py
@@ -3,6 +3,7 @@ from odoo import api, fields, models
 class HospitalAppointment(models.Model):
     _name = 'hospital.appointment'
     _description = 'Hospital Appointment'
+    _rec_name = 'patient_id'
 
     patient_id = fields.Many2one('hospital.patient', string='Patient')
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
